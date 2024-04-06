97ee7fa303570ea6740b94b7429024d6122e1c67
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-04-06 20:58:06 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/BeAuthUserStaffApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.beauthuserstaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAuthUserStaffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAuthUserStaffApplication.class, args);
+    }
+
+}

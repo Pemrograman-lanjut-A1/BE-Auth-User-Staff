1c24dfbcadf3b06f9f269a598cfe7448aa37ada2
DaWanAnOnli <joel.rompis@gmail.com>
2024-04-22 10:37:20 +0700

    [GREEN] Implement Announcement Model

--- src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/model/Announcement.java
+++ src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/model/Announcement.java
@@ -1,18 +1,17 @@
 package id.ac.ui.cs.advprog.beauthuserstaff.model;
 
+
+
+import lombok.Getter;
+
+@Getter
 public class Announcement {
     String id;
     String content;
 
     public Announcement(String id, String content){
-    }
-
-    public String getId(){
-        return null;
-    }
-
-    public String getContent() {
-        return null;
+        this.id = id;
+        this.content = content;
     }
 }
 

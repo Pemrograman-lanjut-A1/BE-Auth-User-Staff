c3f7bb51ecb3783358e7ca20bd00366c5cda874d
DaWanAnOnli <joel.rompis@gmail.com>
2024-04-23 08:48:51 +0700

    [GREEN] Implement AnnouncementServiceImpl.java

--- src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/service/StaffDashboardService/AnnouncementServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/service/StaffDashboardService/AnnouncementServiceImpl.java
@@ -1,23 +1,35 @@
 package id.ac.ui.cs.advprog.beauthuserstaff.service.StaffDashboardService;
 
 import id.ac.ui.cs.advprog.beauthuserstaff.model.Announcement;
+import id.ac.ui.cs.advprog.beauthuserstaff.repository.AnnouncementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
 public class AnnouncementServiceImpl implements AnnouncementService{
+
+    @Autowired
+    private AnnouncementRepository announcementRepository;
     @Override
     public Announcement createAnnouncement(Announcement announcement) {
-        return null;
+        announcementRepository.addAnnouncement(announcement);
+        return announcement;
     }
 
     @Override
-    public void deleteAnnouncement(String string) {
+    public void deleteAnnouncement(String id) {
+        announcementRepository.deleteAnnouncement(id);
     }
 
     @Override
     public List<Announcement> getAllAnnouncements() {
-        return null;
+        Iterator<Announcement> announcementIterator = announcementRepository.getAllAnnouncements();
+        List<Announcement> allAnnouncements = new ArrayList<>();
+        announcementIterator.forEachRemaining(allAnnouncements::add);
+        return allAnnouncements;
     }
 }

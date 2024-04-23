bdde165e2fc6dd89ef55e93e264c02303482858c
DaWanAnOnli <joel.rompis@gmail.com>
2024-04-23 13:42:53 +0700

    [REFACTOR] Add UUID to Announcement ID

--- src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/model/Announcement.java
+++ src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/model/Announcement.java
@@ -4,13 +4,22 @@ package id.ac.ui.cs.advprog.beauthuserstaff.model;
 
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class Announcement {
     String id;
     String content;
 
     public Announcement(String id, String content){
-        this.id = id;
+        if(id == null){
+            UUID uuid = UUID.randomUUID();
+            this.id = (uuid.toString());
+        }
+        else{
+            this.id = id;
+        }
+
         this.content = content;
     }
 }

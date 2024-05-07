2466aa699f0b392e2252c4cfc5dad1cf0e16c849
DaWanAnOnli <joel.rompis@gmail.com>
2024-05-07 14:22:03 +0700

    [RED] Fix AnnouncementControllerTest to support database by adding mocks

--- src/test/java/id/ac/ui/cs/advprog/beauthuserstaff/announcement/AnnouncementControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/beauthuserstaff/announcement/AnnouncementControllerTest.java
@@ -6,70 +6,72 @@ import id.ac.ui.cs.advprog.beauthuserstaff.model.Announcement;
 import id.ac.ui.cs.advprog.beauthuserstaff.repository.AnnouncementRepository;
 import id.ac.ui.cs.advprog.beauthuserstaff.service.StaffDashboardService.AnnouncementService;
 import id.ac.ui.cs.advprog.beauthuserstaff.service.StaffDashboardService.AnnouncementServiceImpl;
+import jakarta.persistence.EntityManager;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
+@ExtendWith(MockitoExtension.class)
 public class AnnouncementControllerTest {
+
+    @InjectMocks
     AnnouncementController announcementController;
+
+    @Mock
     AnnouncementService announcementService;
-    AnnouncementRepository announcementRepository;
 
     @BeforeEach
     void setUp(){
         announcementController = new AnnouncementController();
-        announcementService = new AnnouncementServiceImpl();
-        announcementRepository = new AnnouncementRepository();
 
-        ReflectionTestUtils.setField(announcementService, "announcementRepository", announcementRepository);
         ReflectionTestUtils.setField(announcementController, "announcementService", announcementService);
     }
 
     @Test
     void testCreateAnnouncement() throws org.springframework.boot.configurationprocessor.json.JSONException {
+        Announcement announcement = new Announcement("1", "{\"content\":\"hello\"}");
+        when(announcementService.createAnnouncement(any())).thenReturn(announcement);
         announcementController.createAnnouncement( "{\"content\":\"hello\"}");
-        List<Announcement> resultList = announcementService.getAllAnnouncements();
-        assertEquals(1, resultList.size());
-
-        Announcement resultAnnouncement = resultList.getFirst();
-        assertEquals("hello", resultAnnouncement.getContent());
+        verify(announcementService, times(1)).createAnnouncement(any());
     }
 
     @Test
     void testDeleteAnnouncement() throws org.springframework.boot.configurationprocessor.json.JSONException {
-        announcementController.createAnnouncement("{\"content\":\"hello\"}");
-        List<Announcement> tempList = announcementService.getAllAnnouncements();
-        assertEquals(1, tempList.size());
-
-        Announcement tempAnnouncement = tempList.getFirst();
-        String tempId = tempAnnouncement.getId();
-        String jsonId = "{\"id\":\"" + tempId + "\"}";
-
-        announcementController.deleteAnnouncement(jsonId);
-        List<Announcement> result = announcementService.getAllAnnouncements();
-        assertTrue(result.isEmpty());
+        announcementController.deleteAnnouncement("{\"id\":\"" + "1" + "\"}");
+        verify(announcementService, times(1)).deleteAnnouncement(any());
     }
 
 
     @Test
     void testGetAllAnnouncements() throws JsonProcessingException, JSONException, org.springframework.boot.configurationprocessor.json.JSONException {
-        announcementController.createAnnouncement("{\"content\":\"hello\"}");
-        announcementController.createAnnouncement("{\"content\":\"world\"}");
-        String announcements = announcementController.getAllAnnouncements();
-        JSONArray jsonArray = new JSONArray(announcements);
-        JSONObject announcementJson1 = jsonArray.getJSONObject(0);
-        JSONObject announcementJson2 = jsonArray.getJSONObject(1);
-        assertEquals("hello", announcementJson1.getString("content"));
-        assertEquals("world", announcementJson2.getString("content"));
+        Announcement announcement1 = new Announcement();
+        Announcement announcement2 = new Announcement();
+
+        List<Announcement> announcementList = new ArrayList<>();
+
+        announcementList.add(announcement1);
+        announcementList.add(announcement2);
+
+        when(announcementService.getAllAnnouncements()).thenReturn(announcementList);
+
+        String result = announcementController.getAllAnnouncements();
+        verify(announcementService, times(1)).getAllAnnouncements();
     }
 }

d75569c0159fb59dc450eb3071a19bd8c54e6cba
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-05-12 14:46:16 +0700

    [GREEN] Implement UserService

---
+++ src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/authmodule/service/UserService.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.beauthuserstaff.authmodule.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+    UserDetailsService userDetailsService();
+}

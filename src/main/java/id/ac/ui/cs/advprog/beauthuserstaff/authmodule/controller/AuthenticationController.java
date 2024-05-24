c8b2355a6ccde4a42ec90e4d2b4ae76fd5a1b857
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-05-25 01:32:25 +0700

    [REFACTOR] Add Origin.

--- src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/authmodule/controller/AuthenticationController.java
+++ src/main/java/id/ac/ui/cs/advprog/beauthuserstaff/authmodule/controller/AuthenticationController.java
@@ -24,7 +24,7 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @RestController
-@CrossOrigin(origins = {"http://localhost:8080", " http://34.142.244.77", "https://fe-repo-inky.vercel.app"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8080", " http://34.142.244.77", "https://fe-repo-inky.vercel.app"})
 public class AuthenticationController  {
     private AuthService authService;
 
